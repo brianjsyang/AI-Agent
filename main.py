9906df79beefbd84005e3e821fd3d001ef82aec5
Brian Yang <brianjsyang@gmail.com>
2026-04-20 21:49:34 -0700

    Enable chained communications. Add verbose tag

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -15,21 +16,27 @@ def main():
     # Parse user input
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     # GEMINI client
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-2.5-flash-lite",
-        contents=args.user_prompt,
+        contents=messages,
     )
 
     if not response.usage_metadata:
         raise RuntimeError("Gemini API Request Failed: No usage_metadata returned")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print("Response:")
     print(response.text)
 
